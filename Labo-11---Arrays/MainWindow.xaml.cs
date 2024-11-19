3400f1303012a89001df2c47f8398acf50059af5
Suyen Vanni <12400458@student.pxl.be>
2024-11-19 18:09:24 +0100

    klaar

--- Labo-11---Arrays/MainWindow.xaml.cs
+++ Labo-11---Arrays/MainWindow.xaml.cs
@@ -19,6 +19,48 @@ namespace Labo_11___Arrays
         public MainWindow()
         {
             InitializeComponent();
+            
+
         }
+        //Deel 1
+        private void SearchNumber_Click(object sender, RoutedEventArgs e)
+        {
+            int[] numbers = new int[6];
+            numbers = [100, 50, 20, 60, 90, 80];
+            int[] result;
+
+            result = ReturnSmallestAndBiggest(numbers);
+
+            outputTextBox.Text = $"int[] result = {{ {result[0]}, {result[1]}}}";
+
+            //int smallestNumber = numbers[0];
+            //int biggestNumber = numbers[0];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] < smallestNumber)
+            //    {
+            //        smallestNumber = numbers[i];
+            //    }
+            //    if (numbers[i] > biggestNumber)
+            //    {
+            //        biggestNumber = numbers[i];
+            //    }
+            //}
+            //outputTextBox.Text = $"In totaal zijn er {numbers.Length} getallen:\nKleinste getal = {smallestNumber}\nGrootste getal = 100";
+        }
+
+        private int[] ReturnSmallestAndBiggest(int[] numbers)
+        {
+            int smallest;
+            int biggest;
+            smallest = numbers.Min();
+            biggest = numbers.Max();
+
+            int[] smallestAndBiggest = { smallest, biggest};
+
+            return smallestAndBiggest;
+        }
+
     }
 }
